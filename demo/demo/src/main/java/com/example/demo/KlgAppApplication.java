6aef25cf01b5d51d173260c44b09e7983ea5322c
mateusz-dukat <matidukati@wp.pl>
2026-02-14 20:39:08 +0100

    spring init

---
+++ demo/demo/src/main/java/com/example/demo/KlgAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlgAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlgAppApplication.class, args);
+	}
+
+}
